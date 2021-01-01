4e36c8f5cc359b6d3e66008221da9a03b0374b00
littlejak20 <littlejak22@gmail.com>
2021-01-01 04:11:08 +0100

    Create leitstellenspiel-auswertung.js

---
+++ leitstellenspiel-auswertung.js
@@ -0,0 +1,83 @@
+// ==UserScript==
+// @name         Leitstellenspiel-Auswertung
+// @namespace    https://github.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       littlejak20, jubjub29
+// @match        https://www.leitstellenspiel.de/credits*
+// @grant        none
+// @require      http://code.jquery.com/jquery-3.5.1.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var strLsName = 'test123456';
+    var strLsStartedName = strLsName+'Started';
+    var strTableSelector = 'table.table.table-striped';
+    var strBackButtonSelector = strTableSelector+' + ul.pagination.pagination li.prev > a';
+    var intTimeoutMilliSec = 10;
+    var downloadFileName = 'Einzeltransaktionen.csv';
+
+    console.log('strLsStartedName', localStorage.getItem(strLsStartedName));
+    if (localStorage.getItem(strLsStartedName) === 'true') {
+        generateCSV();
+        return;
+    }
+
+    var startButtonElement = $('<a href="#">START</a>');
+    startButtonElement.on('click.startButton', function(e) {
+        e.preventDefault();
+        localStorage.setItem(strLsName, '');
+        localStorage.setItem(strLsStartedName, 'true');
+        generateCSV();
+    });
+    $('body').append(startButtonElement);
+
+    function generateCSV() {
+        var data = localStorage.getItem(strLsName);
+        if (data === null || data === undefined) data = '';
+
+        $(strTableSelector+' tr').each((index, row) => {
+            if (index <= 0) return;
+            console.log('tr', index, row);
+
+            var arrTds = $(row).find('td');
+
+            arrTds.each((index, td) => {
+                console.log('td', index, td);
+                data += td.innerHTML+';';
+            });
+            data += '\n';
+        });
+
+        console.log(data);
+
+        localStorage.setItem(strLsName, data);
+
+        if ($(strBackButtonSelector).length <= 0) {
+            localStorage.setItem(strLsStartedName, 'false');
+            generateTextarea();
+            generateCSVFile();
+            return true;
+        }
+        setTimeout(() => {
+            window.location = $(strBackButtonSelector).attr('href');
+        }, intTimeoutMilliSec);
+        return false;
+    }
+
+    function generateTextarea() {
+        $('body').append($('<textarea>'+localStorage.getItem(strLsName)+'</textarea>'));
+    }
+
+    function generateCSVFile() {
+        var strFileContent = 'data:text/csv;charset=utf-8,'+localStorage.getItem(strLsName);
+        var uriFileContent = encodeURI(strFileContent);
+        var downloadButtonElement = $('<a>Download</a>');
+        downloadButtonElement.attr('href', uriFileContent);
+        downloadButtonElement.attr('download', downloadFileName);
+        $('body').append(downloadButtonElement);
+
+    }
+})();
